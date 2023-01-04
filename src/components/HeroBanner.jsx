00c7c61a0c2017a01f10a18a42ac3c0a25358c45
juhayerayaz <aiazj97@gmail.com>
2023-01-05 03:14:28 +0600

    heroBanner and achivement header added

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
-const HeroBanner = () => {
+const HeroBanner = ({title,heroText}) => {
     return (
-        <div>
-            
+    <div>
+        <div className='bg-fixed h-[280px] relative'
+            style={{
+            background: 'url("assets/images/aboutbg3.jpg") top center no-repeat',
+            }}
+        >
+            <div className='bg-black h-[280px] bg-opacity-[.78]'>
+                <div className='relative z-20'>
+                    <div className='text-center pt-[150px] pb-8'>
+                        <div className='text-4xl text-white font-light leading-7 font-mont'>{title}</div>
+                        <br />
+                        <em className='text-[#999999] font-play leading-[16px] italic'>{heroText}</em>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['Montserrat', 'sans-serif'],
+        play: ['Playfair Display', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
