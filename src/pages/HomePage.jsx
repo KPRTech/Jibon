7910ee92d8fcce0ac346530688e81e8a4ba09e76
fasbender <alamgirfaisalbin@gmail.com>
2023-01-05 02:29:26 +0600

    scrollToTop and messagerChat added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>
+    <div style={{height: '150vh'}}>
       <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
     </div>
   );
