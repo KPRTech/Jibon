0b9681b7ce2b0e712163044b601794dcec14d6e9
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2023-01-05 14:17:27 +0600

    navbar scroll animation added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos < 70) ||
+        currentScrollPos < 200
     );
-}
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
+  const navbarStyles = {
+    position: "fixed",
+    height: "60px",
+    width: "100%",
+    backgroundColor: "grey",
+    textAlign: "center",
+    transition: "top 0.6s",
+  };
+
+  return (
+    <div>
+      <div style={{ ...navbarStyles, top: visible ? "0" : "-60px" }}>
+        Some Company Inc.
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div className="mt-20">sdfsd</div>;
 };
 
 export default HomePage;
