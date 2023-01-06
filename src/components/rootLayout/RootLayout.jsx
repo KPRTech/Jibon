f315bc25740498d233121297264574e163656765
syedmehedi0003 <mehedihasanbahar.com>
2023-01-07 00:14:38 +0600

    added make donate

--- src/components/rootLayout/RootLayout.jsx
+++ src/components/rootLayout/RootLayout.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
-import Footer from '../Footer';
+import Footer from "../Footer";
 import ScrollToTop from "../ui/ScrollToTop ";
-
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      <Footer/>
-      <ScrollToTop/>
+      <Footer />
+      <ScrollToTop />
     </>
   );
 };
