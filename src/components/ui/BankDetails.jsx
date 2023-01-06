3156d4758cbc76dc8bc03be281802e200486494e
Arahman75 <selfarahman01825@gmail.com>
2023-01-07 00:55:44 +0600

    add a our bank component

--- src/components/ui/BankDetails.jsx
+++ src/components/ui/BankDetails.jsx
@@ -7,7 +7,7 @@ const BankDetails = () => {
          <div className='container grid grid-rows-1 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:mx-24 lg:pt-7 lg:pb-4 py-4 px-4'>
             <div>
                 <h2 className='text-xl font-bold text-[#FF6400]'>1. HABIB BANK LTD.</h2>
-                <h3 className='leading-8 text-gray-700 text-lg'><span className='text-black font-bold'>Title of Account:</span> Zindagi Trust</h3>
+                <h3 className='leading-8 text-gray-700 text-lg tracking-normal'><span className='text-black font-bold'>Title of Account:</span> Zindagi Trust</h3>
                 <h3 className='leading-8 text-gray-700'><span className='text-black font-bold'>Bank Name:</span> Habib Bank Limited</h3>
             </div>
 
