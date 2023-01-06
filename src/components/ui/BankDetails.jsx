092af4858fdd2bf1a2b756b4a04d6a7a5da1e5e5
Arahmna75 <selfaraman01825@gmail.com>
2023-01-06 16:45:58 +0600

    create a bank-details section

---
+++ src/components/ui/BankDetails.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const BankDetails = () => {
+    return (
+        <div>
+         <div className='container grid grid-rows-1 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 lg:mx-24 lg:pt-7 lg:pb-4 py-4 px-4'>
+            <div>
+                <h2 className='text-xl font-bold text-[#FF6400]'>1. HABIB BANK LTD.</h2>
+                <h3 className='leading-8 text-gray-700 text-lg'><span className='text-black font-bold'>Title of Account:</span> Zindagi Trust</h3>
+                <h3 className='leading-8 text-gray-700'><span className='text-black font-bold'>Bank Name:</span> Habib Bank Limited</h3>
+            </div>
+
+            <div>
+                <h2>1. HABIB BANK LTD.</h2>
+            </div>
+            <div>
+                <h2>1. HABIB BANK LTD.</h2>
+            </div>
+            <div>
+                <h2>1. HABIB BANK LTD.</h2>
+            </div>
+            <div>
+                <h2>1. HABIB BANK LTD.</h2>
+            </div>
+
+         </div>
+        </div>
+    );
+};
+
+export default BankDetails;
