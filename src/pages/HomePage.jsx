0a5bc1dc1464de208b0ca48d0a871268fddd3020
fasbender <alamgirfaisalbin@gmail.com>
2023-01-07 00:18:09 +0600

    primarySlider added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ShowBanner from "../components/ShowBanner";
+import PrimarySlider from "../components/PrimarySlider";
 
 const HomePage = () => {
   return (
     <div>
       <ShowBanner />
-      <div className="h-screen"></div>
+      {/* <div className="h-screen"></div> */}
+      <PrimarySlider/>
     </div>
   )
 };
