be0b54e186b9e3ba4ad3dd93c0e82d99453dbe19
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2023-01-07 16:28:46 +0600

    favicon added

--- src/components/BeforeAfterSlider.jsx
+++ src/components/BeforeAfterSlider.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
-import ReactBeforeSliderComponent from 'react-before-after-slider-component';
-import 'react-before-after-slider-component/dist/build.css';
-import AchievementHeader from './AchievementHeader';
+import React from "react";
+import ReactBeforeSliderComponent from "react-before-after-slider-component";
+import "react-before-after-slider-component/dist/build.css";
+import AchievementHeader from "./AchievementHeader";
 
 const BeforeAfterSlider = () => {
+  const FIRST_IMAGE = {
+    imageUrl: "https://www.zindagitrust.org/img/sections/school1.jpg",
+  };
+  const SECOND_IMAGE = {
+    imageUrl: "https://www.zindagitrust.org/img/sections/school2new.jpg",
+  };
 
-    const FIRST_IMAGE = {
-  imageUrl: 'https://www.zindagitrust.org/img/sections/school1.jpg'
-};
-const SECOND_IMAGE = {
-  imageUrl: 'https://www.zindagitrust.org/img/sections/school2new.jpg'
-};
-
+  const delimiterIconStyles = {
+    width: "40px",
+    height: "40px",
+    backgroundSize: "cover",
+    borderRadius: "none",
+    backgroundColor: "#FF6400",
+    backgroundImage: "url(<some-path-here>)",
+  };
 
   return (
-    <div className='relative'>
-        <AchievementHeader achiveHeader="BEFORE AND AFTER" achiveDescription="Hold and slide right to see the school transform from before to after our intervention" />
-        <div className='lg:w-[35%] md:w-[60%] m-auto lg:p-4 md:p-3 p-2'>
-            <ReactBeforeSliderComponent
-                firstImage={FIRST_IMAGE}
-                secondImage={SECOND_IMAGE}
-            />
-        </div>
+    <div className="relative">
+      <AchievementHeader
+        achiveHeader="BEFORE AND AFTER"
+        achiveDescription="Hold and slide right to see the school transform from before to after our intervention"
+      />
+      <div className="lg:w-[55%] md:w-[60%] m-auto lg:p-4 md:p-3 p-2">
+        <ReactBeforeSliderComponent
+          firstImage={FIRST_IMAGE}
+          secondImage={SECOND_IMAGE}
+          withResizeFeel={true}
+          delimiterIconStyles={delimiterIconStyles}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BeforeAfterSlider
+export default BeforeAfterSlider;
