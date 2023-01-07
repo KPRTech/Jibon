97d12acfb258cf976f155e42a375fc303f898fe9
syedmehedi0003 <mehedihasanbahar.com>
2023-01-07 12:28:37 +0600

    addes what-we -do

--- src/components/rootLayout/RootLayout.jsx
+++ src/components/rootLayout/RootLayout.jsx
@@ -3,11 +3,17 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import ScrollToTop from "../ui/ScrollToTop ";
+import WhatWeDo from "../WhatWeDo/WhatWeDo";
+import Advocacy from "../Advocacy";
+import Organizetion from "../Organizetion";
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
+      <WhatWeDo></WhatWeDo>
+      <Advocacy></Advocacy>
+      <Organizetion></Organizetion>
       <Outlet />
       <Footer />
       <ScrollToTop />
