487a5231f6869717e03a7eccad3bedc5aabf51d8
Arahman75 <selfarahman01825@gmail.com>
2023-01-07 10:26:12 +0600

    complete out bank details

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import ShowBanner from "../components/ShowBanner";
 import PrimaryButton from "../components/PrimaryButton";
 import UpperFooter from "../components/UpperFooter";
 import PrimarySlider from "../components/PrimarySlider";
+import BankDetails from "../components/ui/BankDetails";
 
 const HomePage = () => {
   return (
     <div>
         <ShowBanner />
         <PrimarySlider />
+        <BankDetails></BankDetails>
         <div>
           <UpperFooter>
           <div className="lg:mx-24 lg:pt-7 lg:pb-4 py-4 px-4 lg:flex lg:text-left text-center lg:justify-between">
