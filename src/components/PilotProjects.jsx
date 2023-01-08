10e4ee68545fe33e84054892fe6462efd07b6531
fasbender <alamgirfaisalbin@gmail.com>
2023-01-08 19:17:47 +0600

    schoolReform complete

--- src/components/PilotProjects.jsx
+++ src/components/PilotProjects.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const PilotProjects = () => {
   return (
-    <div className='p-3 lg:px-[45px] lg:pt-[30px] lg:w-[80%] m-auto text-center overflow-auto'>
+    <div className='p-3 lg:px-[45px] lg:pt-[30px] lg:w-[80%] m-auto text-center'>
         <h2 className='text-2xl font-bold text-[#ff6400] mt-5'>PILOT PROJECTS</h2>
         <p className='font-light mb-10'>We have adopted two government schools as pilot projects of school reform involving interventions in infrastructure upgrades, governance, administration, teacher professional development, academic innovation and tracking and a rich menu of co-curricular and extra-curricular activities.</p>
         <h1 className='mb-10 text-3xl font-bold'>OUR SCHOOLS</h1>
-        <div style={{gridTemplateColumns: '200px 1fr 1fr'}} className='grid grid-rows-6 text-center mb-5'>
+        <div style={{gridTemplateColumns: '200px 1fr 1fr'}} className='grid grid-rows-6 overflow-auto text-center mb-5'>
             <div className='invisible pointer-events-none'></div>
             <div className='bg-gradient-to-b from-[#9ED82B] to-[#95D131] lg:p-3 md:p-1 rounded-t-xl text-white font-bold'>SMB Fatima Jinnah Government Girls School</div>
             <div className='bg-gradient-to-b from-[#9ED82B] to-[#95D131] lg:p-3 md:p-1 rounded-t-xl text-white font-bold'>Khatoon-e-Pakistan Government Girls School</div>
