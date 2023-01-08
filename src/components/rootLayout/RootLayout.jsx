3251eb98cae2d05566f0e244559087ca459ccd79
Nafis Abdullah <n.ahmad.web.cit22@gmail.com>
2023-01-08 11:15:39 +0600

    updated

--- src/components/rootLayout/RootLayout.jsx
+++ src/components/rootLayout/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer";
 import NavBar from "../NavBar";
+import Footer from "../Footer";
 import ScrollToTop from "../ui/ScrollToTop ";
 
 const RootLayout = () => {
