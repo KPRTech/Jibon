48165903dc21646fc1867b9e58270e76948728bd
syedmehedi0003 <mehedihasanbahar.com>
2023-01-09 12:21:02 +0600

    financials

--- src/components/Financial.jsx
+++ src/components/Financial.jsx
@@ -9,12 +9,12 @@ const Financial = () => {
           <div className="grid justify-items-center p-2 text-justify lg:mx-2">
             <img
               src="/assets/images/Finalcials/img1.jpeg"
-              className="mb-[0px]"
+              className="h-[100%] pb-4"
               alt=""
             />
             <a
               className="h-[22px] text-[#ff6400]  tracking-wide 
-              hover:border-solid hover:border-b-2 hover:border-orange-600"
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
               href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
               target="_blank"
             >
@@ -25,36 +25,125 @@ const Financial = () => {
           {/* two  */}
 
           <div className="grid justify-items-center p-2 text-justify lg:mx-2">
-            <img src="/assets/images/LeaderShip/mahnoor.png" alt="" />
-            <h2 className="text-[#333333] text-[17px]  py-[10px] font-bold">
-              Mahnoor Mahboob
-            </h2>
-            <p className="text-[#333333] text-[15px] leading-6 pb-[30px]">
-              Program Officer - Special Programs & Advocacy
-            </p>
+            <img
+              src="/assets/images/Finalcials/img2.jpeg"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
           </div>
 
           {/* three  */}
           <div className="grid justify-items-center p-2 text-justify lg:mx-2">
-            <img src="/assets/images/LeaderShip/ahsan.png" alt="" />
-            <h2 className="text-[#333333] text-[17px]  py-[10px] font-bold">
-              Ahsan Abdul Razzak
-            </h2>
-            <p className="text-[#333333] text-[15px] leading-6 pb-[30px]">
-              Marketing & Communication Officer
-            </p>
+            <img
+              src="/assets/images/Finalcials/img3.jpg"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
           </div>
 
           {/* four  */}
 
           <div className="grid justify-items-center p-2 text-justify lg:mx-2">
-            <img src="/assets/images/LeaderShip/ahsan.png" alt="" />
-            <h2 className="text-[#333333] text-[17px]  py-[10px] font-bold">
-              Ahsan Abdul Razzak
-            </h2>
-            <p className="text-[#333333] text-[15px] leading-6 pb-[30px]">
-              Marketing & Communication Officer
-            </p>
+            <img
+              src="/assets/images/Finalcials/img4.png"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
+          </div>
+        </div>
+        {/* 1st part end  */}
+
+        <div className="lg:flex mt-10">
+          {/* one  */}
+          <div className="grid justify-items-center p-2 text-justify lg:mx-2">
+            <img
+              src="/assets/images/Finalcials/img5.png"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
+          </div>
+
+          {/* two  */}
+          <div className="grid justify-items-center p-2 text-justify lg:mx-2">
+            <img
+              src="/assets/images/Finalcials/img6.png"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
+          </div>
+
+          {/* three  */}
+          <div className="grid justify-items-center p-2 text-justify lg:mx-2">
+            <img
+              src="/assets/images/Finalcials/img7.png"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
+          </div>
+
+          {/* four  */}
+          <div className="grid justify-items-center p-2 text-justify lg:mx-2">
+            <img
+              src="/assets/images/Finalcials/img8.png"
+              className="h-[100%] pb-4"
+              alt=""
+            />
+            <a
+              className="h-[22px] text-[#ff6400]  tracking-wide 
+              hover:border-solid hover:border-b-2 hover:border-[#ff6400]"
+              href="/annualreports/Financial-Highlights-2021-Zindagi-Trust-Updated.pdf"
+              target="_blank"
+            >
+              DOWNLOAD REPORT
+            </a>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const NavBar = () => {
               }
               linkName={"Financials"}
               linkClass={"after:hidden"}
-              href={"#"}
+              href={"/about-us/financials"}
             />
           </Dropdown>
         </NavItem>
