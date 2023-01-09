33648ec2823eac71526ff0a6b5dba1f6c772ca5d
fasbender <alamgirfaisalbin@gmail.com>
2023-01-09 17:35:00 +0600

    advocacy more content added

--- src/components/HistoryVision.jsx
+++ src/components/HistoryVision.jsx
@@ -21,19 +21,19 @@ const HistoryVision = () => {
     };
 
   return (
-    <div className='grid lg:grid-cols-2 grid-cols-1 lg:w-[80%] m-auto lg:p-[4rem] py-[3rem]'>
+    <div className='grid lg:grid-cols-2 grid-cols-1 lg:w-[80%] w-fit m-auto lg:p-[4rem] py-[3rem]'>
         <div className='lg:px-10 px-5 lg:mb-0 mb-5'>
             <h1 className='lg:py-4 text-[#FF6400] font-bold text-2xl'>HISTORY AND VISION</h1>
             <hr />
             <p className='lg:py-4'>We work to change policy to reform schools through advocacy with the political leadership, the government including the top bureaucracy and mid-level officers and supporting organisations, civil society and other key opinion leaders and influencers.</p>
             <p className='lg:py-4'>Based on our learnings and successes at our schools, we have effectively brought change to policies in school governance and child protection.</p>
         </div>
-        <div className='lg:w-[60%] md:w-[50%] w-[95%] m-auto'>
+        <div className='lg:w-[60%] md:w-[50%] w-full m-auto lg:p-0 p-2'>
             <Slider {...settings}>
                 {vision_slider.map((vision, index) => {
                     return (
                         <div key={index}>
-                            <img src={vision.img} alt='' />
+                            <img className='' src={vision.img} alt='' />
                         </div>
                     )
                 })}
