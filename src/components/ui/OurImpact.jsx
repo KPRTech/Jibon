ae8bb98ad4afebebda944d8bd5d67c09e527e3a6
tanvir <shovontanvir124@gmail.com>
2023-01-10 12:51:15 +0600

    added school reform page link and our impact pages to to home page

--- src/components/ui/OurImpact.jsx
+++ src/components/ui/OurImpact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import AchievementHeader from "../AchievementHeader";
+import { Link } from "react-router-dom";
+
 
 
 const OurImpact = () => {
@@ -10,21 +12,21 @@ const OurImpact = () => {
         achiveDescription="Our impact includes revamped and new school facilities; individual student achievements in academics and beyond; the introduction of programs like art, sports and chess; student wellness initiatives such as breakfast, healthcare and counseling; teacher professional development; customised blended learning through software; and, most importantly, change in national and provincial education policy."
       />
       <div className="container grid grid-rows-1 lg:grid-cols-5 md:gap-2 sm:text-center md:grid-cols-2 grid-cols-1 lg:gap-4 gap-y-4 lg:py-12 py-4 px-4 ">
-        <a href="https://www.zindagitrust.org/impact/infrastructure">
+        <Link to="/infrastructure">
           <img src='assets/images/impact/impact-img1.jpg' alt="" />
-        </a>
-        <a href="https://www.zindagitrust.org/impact/governance">
+        </Link>
+        <Link to="/governance">
           <img src='assets/images/impact/impact-img2.jpg' alt="" />
-        </a>
-        <a href="https://www.zindagitrust.org/impact/new-programs">
+        </Link>
+        <Link to="/impact/new-program">
           <img src='assets/images/impact/impact-img3.jpg' alt="footer-img2" />
-        </a>
-        <a href="https://www.zindagitrust.org/impact/academics">
+        </Link>
+        <Link to="/impact/academics">
           <img src='assets/images/impact/impact-img4.jpg' alt="" />
-        </a>
-        <a href="https://www.zindagitrust.org/impact/policy">
+        </Link>
+        <Link to="/impact/policy">
           <img src='assets/images/impact/impact-img5.jpg' alt="" />
-        </a>
+        </Link>
       </div>
     </div>
   );
