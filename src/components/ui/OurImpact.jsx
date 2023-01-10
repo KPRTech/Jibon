81eeb17e47b0cf746461af81bb7369a5345714cd
tanvir <shovontanvir124@gmail.com>
2023-01-10 13:17:40 +0600

    fixed our impact component align issue

--- src/components/ui/OurImpact.jsx
+++ src/components/ui/OurImpact.jsx
@@ -6,12 +6,12 @@ import { Link } from "react-router-dom";
 
 const OurImpact = () => {
   return (
-    <div className="mx-24">
+    <div className="">
       <AchievementHeader
         achiveHeader="OUR IMPACT"
         achiveDescription="Our impact includes revamped and new school facilities; individual student achievements in academics and beyond; the introduction of programs like art, sports and chess; student wellness initiatives such as breakfast, healthcare and counseling; teacher professional development; customised blended learning through software; and, most importantly, change in national and provincial education policy."
       />
-      <div className="container grid grid-rows-1 lg:grid-cols-5 md:gap-2 sm:text-center md:grid-cols-2 grid-cols-1 lg:gap-4 gap-y-4 lg:py-12 py-4 px-4 ">
+      <div className="flex flex-row flex-wrap justify-center gap-20 mt-10">
         <Link to="/infrastructure">
           <img src='assets/images/impact/impact-img1.jpg' alt="" />
         </Link>
